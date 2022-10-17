9c8d65e472380f41a923edde3bf100e2b89555df
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-17 13:54:48 +0000

    Initial Commit

---
+++ Pinch App/Pinch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinch_AppApp.swift
+//  Pinch App
+//
+//  Created by Mary Moreira on 17/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Pinch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
